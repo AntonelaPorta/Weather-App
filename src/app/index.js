4d898125b3034669462c4f45321100272956711d
Antonela Porta <porta.antonela@gmail.com>
2022-10-01 19:47:29 -0300

    Creamos la clase UI que se encarga de renderizar la data de la api

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,14 @@
 require('./index.css')
 const { Weather } = require('./Weather')
+const { UI } = require('./UI')
+
+const ui = new UI()
 const weather = new Weather('London', 'uk')
 
 async function fetchWeather() {
     const data = await weather.getWeather()
     console.log(data)
+    ui.render(data)
 }
 
 document.addEventListener('DOMContentLoaded', fetchWeather)
